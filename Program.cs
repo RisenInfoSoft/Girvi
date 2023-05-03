644e5ed5006e2e49375467b15f53d39945d06e8c
RisenInfoSoft <132468722+RisenInfoSoft@users.noreply.github.com>
2023-05-03 19:34:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Girvi.Forms.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Girvi
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmdesktop());
+        }
+    }
+}
